89b0619e75abc267bd0db483393268c81593f315
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-03-21 19:17:32 -0300

    fix: array context useEffect missing dependency

--- src/context/ArrayContext.tsx
+++ src/context/ArrayContext.tsx
@@ -1,55 +1,59 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
 interface IArrayContext {
-	array: number[];
-	changeArray: (array: number[]) => void;
-	doSwap: (i: number, j: number) => void;
-	randomizeArray: () => void;
+  array: number[];
+  changeArray: (array: number[]) => void;
+  doSwap: (i: number, j: number) => void;
+  randomizeArray: () => void;
 }
 
 export const ArrayContext = createContext<IArrayContext>({
-	array: [],
-	changeArray: () => true,
-	doSwap: () => true,
-	randomizeArray: () => true,
+  array: [],
+  changeArray: () => true,
+  doSwap: () => true,
+  randomizeArray: () => true,
 });
 
 export const ArrayContextProvider: React.FC = ({ children }) => {
-	const [array, setArray] = useState([] as number[]);
+  const [array, setArray] = useState([] as number[]);
 
-	useEffect(() => {
-		randomizeArray();
-	}, []);
+  useEffect(() => {
+    const newArray = [] as number[];
+    for (let i = 0; i < 100; i++) newArray.push(randomNumber());
+    setArray(newArray);
+  }, []);
 
-	function changeArray(newArray: number[]) {
-		setArray(newArray);
-	}
+  function changeArray(newArray: number[]) {
+    setArray(newArray);
+  }
 
-	function doSwap(i: number, j: number) {
-		const aux = array[i];
-		array[i] = array[j];
-		array[j] = aux;
-		setArray([...array]);
-	}
+  function doSwap(i: number, j: number) {
+    const aux = array[i];
+    array[i] = array[j];
+    array[j] = aux;
+    setArray([...array]);
+  }
 
-	function randomizeArray() {
-		const newArray = [] as number[];
-		const newLength = array.length > 0 ? array.length : 100;
+  function randomizeArray() {
+    const newArray = [] as number[];
+    const newLength = array.length > 0 ? array.length : 100;
 
-		for (let i = 0; i < newLength; i++) newArray.push(randomNumber());
+    for (let i = 0; i < newLength; i++) newArray.push(randomNumber());
 
-		setArray(newArray);
-	}
+    setArray(newArray);
+  }
 
-	return (
-		<ArrayContext.Provider value={{ array, changeArray, doSwap, randomizeArray }}>
-			{children}
-		</ArrayContext.Provider>
-	);
+  return (
+    <ArrayContext.Provider
+      value={{ array, changeArray, doSwap, randomizeArray }}
+    >
+      {children}
+    </ArrayContext.Provider>
+  );
 };
 
 function randomNumber() {
-	const min = Math.ceil(5);
-	const max = Math.floor(500);
-	return Math.floor(Math.random() * (max - min) + min);
+  const min = Math.ceil(5);
+  const max = Math.floor(500);
+  return Math.floor(Math.random() * (max - min) + min);
 }
