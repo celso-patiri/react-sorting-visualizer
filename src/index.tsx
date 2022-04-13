c2ba7607af91050d2cc50efae84abea3ca3a1fa7
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-13 16:10:26 -0300

    test: fugitive integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-
-import './styles/index.css';
-
+import React from "react";
+import "./styles/index.css";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+//hello my friend
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
